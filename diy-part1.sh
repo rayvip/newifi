314bfce944fdabcf10d91a768b4bf8a42a5d94a7
rayvip <35946678+rayvip@users.noreply.github.com>
2020-06-13 17:01:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#=============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#=================================================
+# Description: DIY script
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
-#=============================================================
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#移除不用软件包
+rm -rf k3screenctrl package/lean
+#添加额外软件包
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/xnxy2012/luci-app-passwall.git
+git clone https://github.com/vernesong/OpenClash.git
